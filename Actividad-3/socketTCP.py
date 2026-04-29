68656a052daaea10de95f85564cdc77cfbb9a252
DeftneSS <lucasbrule01@gmail.com>
2026-04-29 17:00:44 -0400

    diccionario creado en parser

--- Actividad-3/socketTCP.py
+++ Actividad-3/socketTCP.py
@@ -8,15 +8,42 @@ class SocketTCP():
         self.direccionOrigen = None
         self.seq = 0
         self.ack = 0
+        self.fin = 0
 
     @staticmethod
     def parse_segment(segment):
 
-        segment
+        header = segment[0]
+        datos = segment[1:16]
+
+        parsed = {
+            "SYN": 0,
+            "ACK": 0,
+            "FIN": 0,
+        }
+
+        if  (header == 4):
+            parsed["SYN"] = 1
+
+        if (header == 2):
+            parsed["ACK"] = 1
+
+        if (header == 6):
+            parsed["SYN"] = 1
+            parsed["ACK"] = 1
+
+        if (header == 1):
+            parsed["FIN"] = 1
+
+        if (header == 3):
+            parsed["FIN"] = 1
+            parsed["ACK"] = 1
+
+        if (header == 0):
+            parsed["DATOS"] = 1
 
 
-        return
-
+        return parsed
 
     def create_segment(parsed_segment):
 
