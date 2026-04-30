0e63b39f690aede32033e1dfe4ad0244e9e7dce7
lucas.brule@ug.uchile.cl <lucasbrule01@gmail.com>
2026-04-30 13:23:46 -0400

    feat: implementacion 1 socketTCP punto 3

--- Actividad-3/socketTCP.py
+++ Actividad-3/socketTCP.py
@@ -14,12 +14,15 @@ class SocketTCP():
     def parse_segment(segment):
 
         header = segment[0]
-        datos = segment[1:16]
+        sequence = segment[1:4]
+        datos = segment[5:20]
 
         parsed = {
             "SYN": 0,
             "ACK": 0,
             "FIN": 0,
+            "SEQ": sequence,
+            "DATOS": datos
         }
 
         if  (header == 4):
@@ -39,14 +42,33 @@ class SocketTCP():
             parsed["FIN"] = 1
             parsed["ACK"] = 1
 
-        if (header == 0):
-            parsed["DATOS"] = 1
-
-
         return parsed
+
 
     def create_segment(parsed_segment):
 
+        segment = bytearray(20)
 
+        if parsed_segment["SYN"] == 1 and parsed_segment["ACK"] == 0 and parsed_segment["FIN"] == 0:
+            header = 4
 
-        return
+        if parsed_segment["SYN"] == 0 and parsed_segment["ACK"] == 1 and parsed_segment["FIN"] == 0:
+            header = 2
+
+        if parsed_segment["SYN"] == 1 and parsed_segment["ACK"] == 1 and parsed_segment["FIN"] == 0:
+            header = 6
+
+        if parsed_segment["SYN"] == 0 and parsed_segment["ACK"] == 0 and parsed_segment["FIN"] == 1:
+            header = 1
+
+        if parsed_segment["SYN"] == 0 and parsed_segment["ACK"] == 1 and parsed_segment["FIN"] == 1:
+            header = 3
+
+        if parsed_segment["SYN"] == 0 and parsed_segment["ACK"] == 0 and parsed_segment["FIN"] == 0:
+            header = 0
+
+        segment[0] = header
+        segment[1:4] = parsed_segment["SEQ"]
+        segment[5:20] = parsed_segment["DATOS"]
+
+        return segment
