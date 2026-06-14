2e01a6b46c18ac57d6dde8a127b94e3d9be73d10
agustinmancilla <agustin2002mp@gmail.com>
2026-06-14 02:50:44 -0400

    Modificado sender para funcionar de la forma paquete ip_destino puerto_destino

--- Actividad-6/sender.py
+++ Actividad-6/sender.py
@@ -3,24 +3,34 @@ import sys
 import router
 
 if __name__ == "__main__":
-    direccion_final = (sys.argv[1], int(sys.argv[2]))
-    direccion_envio = (sys.argv[3], int(sys.argv[4]))
-    mensaje = sys.argv[5]
-    ttl = int(sys.argv[6]) if len(sys.argv) >= 7 else 10
 
+    ip_destino, puerto_destino, ttl, mensaje = sys.argv[1].split(";")
     mensaje_bytes = mensaje.encode()
+    direccion_final = (
+        ip_destino,
+        int(puerto_destino)
+    )
+
+    direccion_envio = (
+        sys.argv[2],
+        int(sys.argv[3])
+    )
 
     parsed_packet = {
         "ip": direccion_final[0],
         "port": direccion_final[1],
-        "ttl": ttl,
-        "id": 1,
+        "ttl": int(ttl),
+        "id": 347,
         "offset": 0,
         "length": len(mensaje_bytes),
         "flag": 0,
         "message": mensaje_bytes
     }
+
     packet = router.create_packet(parsed_packet)
+
     socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     socketUDP.sendto(packet, direccion_envio)
+
     socketUDP.close()
