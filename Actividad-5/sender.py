0e5c1c9a74d443cfe8dd88ba408e781c4a3ed17a
agustinmancilla <agustin2002mp@gmail.com>
2026-06-21 06:31:31 -0400

    Actividad 5 arreglada, habia un error en el sender

--- Actividad-5/sender.py
+++ Actividad-5/sender.py
@@ -3,16 +3,31 @@ import sys
 import router
 
 if __name__ == "__main__":
-    direccion_final = (sys.argv[1], int(sys.argv[2]))
-    direccion_envio = (sys.argv[3], int(sys.argv[4]))
-    mensaje = sys.argv[5]
 
-    parsed_packet = {
-        "ip": direccion_final[0],
-        "port": direccion_final[1],
-        "message": mensaje.encode()
-    }
-    packet = router.create_packet(parsed_packet)
+
+    ip_destino, puerto_destino, ttl = sys.argv[1].split(";")
+
+    direccion_final = (
+        ip_destino,
+        int(puerto_destino)
+    )
+
+    direccion_envio = (
+        sys.argv[2],
+        int(sys.argv[3])
+    )
     socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socketUDP.sendto(packet, direccion_envio)
+    with open("archivo_prueba.txt", "r") as f:
+        for linea in f:
+            mensaje = linea.encode()
+            parsed_packet = {
+            "ip": direccion_final[0],
+            "port": direccion_final[1],
+            "ttl": int(ttl),
+            "message": mensaje
+            }
+            packet = router.create_packet(parsed_packet)
+
+            socketUDP.sendto(packet, direccion_envio)
+
     socketUDP.close()
--- Actividad-5/sender1.py
+++ Actividad-5/sender1.py
@@ -3,30 +3,16 @@ import sys
 import router
 
 if __name__ == "__main__":
-
-    ip_destino, puerto_destino, ttl = sys.argv[1].split(";")
-
-    direccion_final = (
-        ip_destino,
-        int(puerto_destino)
-    )
-
-    direccion_envio = (
-        sys.argv[2],
-        int(sys.argv[3])
-    )
+    direccion_final = (sys.argv[1], int(sys.argv[2]))
+    direccion_envio = (sys.argv[3], int(sys.argv[4]))
+    mensaje = sys.argv[5]
 
     parsed_packet = {
         "ip": direccion_final[0],
         "port": direccion_final[1],
-        "ttl": int(ttl),
-        "message": b"hola"
+        "message": mensaje.encode()
     }
-
     packet = router.create_packet(parsed_packet)
-
     socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     socketUDP.sendto(packet, direccion_envio)
-
     socketUDP.close()
